8f78013ecdc66eaaae6d4a1706852316f1624339
LisPasha <pasha.lissleping@gmail.com>
2021-10-06 23:35:28 +0300

    HabrTelegramBot - Создание каркаса SpringBoot для бота

---
+++ src/test/java/com/example/habrot/habrtelegrambot/HabrTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.habrot.habrtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabrTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
